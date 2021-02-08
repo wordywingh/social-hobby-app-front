aa798bece07d45a4c2a17e5691c0124692f0edec
GitHub User Name <wihumail@gmail.com>
2021-02-08 11:33:14 -0800

    after part 20

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -5,6 +5,7 @@ import {
   CLEAR_ERRORS,
   LOADING_UI,
   SET_UNAUTHENTICATED,
+  LOADING_USER,
 } from "../types";
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -50,6 +51,7 @@ export const logoutUser = () => (dispatch) => {
 };
 
 export const getUserData = () => (dispatch) => {
+  dispatch({ type: LOADING_USER });
   axios
     .get("/user")
     .then((res) => {
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,7 +1,13 @@
-import { SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED } from "../types";
+import {
+  SET_USER,
+  SET_AUTHENTICATED,
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
+} from "../types";
 
 const initialState = {
   authenticated: false,
+  loading: false,
   credentials: {},
   likes: [],
   notifications: [],
@@ -21,6 +27,11 @@ export default function (state = initialState, action) {
         authenticated: true,
         loading: false,
         ...action.payload,
+      };
+    case LOADING_USER:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
