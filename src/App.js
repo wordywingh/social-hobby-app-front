8c23e3ac96de8ff4a23f61aed0d94ec10379c8f8
GitHub User Name <wihumail@gmail.com>
2021-02-19 18:44:03 -0800

    fixed some things for deployment

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
 
+axios.defaults.baseURL =
+  "https://us-central1-socialape-17bac.cloudfunctions.net/api";
+
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
