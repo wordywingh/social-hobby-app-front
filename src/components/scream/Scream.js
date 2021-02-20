e76ebf8174c5289d28be6f6c87ab4842c838b82b
GitHub User Name <wihumail@gmail.com>
2021-02-19 17:01:44 -0800

    after 32

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -84,7 +84,11 @@ export class Scream extends Component {
             <ChatIcon color="primary" />
           </MyButton>
           <span>{commentCount} comments</span>
-          <ScreamDialog screamId={screamId} userHandle={userHandle} />
+          <ScreamDialog
+            screamId={screamId}
+            userHandle={userHandle}
+            openDialog={this.props.openDialog}
+          />
         </CardContent>
       </Card>
     );
@@ -95,6 +99,7 @@ Scream.propTypes = {
   user: PropTypes.object.isRequired,
   scream: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
+  openDialog: PropTypes.bool,
 };
 
 const mapStateToProps = (state) => ({
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -6,6 +6,7 @@ import {
   LOADING_UI,
   SET_UNAUTHENTICATED,
   LOADING_USER,
+  MARK_NOTIFICATIONS_READ,
 } from "../types";
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -79,6 +80,17 @@ export const editUserDetails = (userDetails) => (dispatch) => {
     .post("/user", userDetails)
     .then(() => {
       dispatch(getUserData());
+    })
+    .catch((err) => console.log(err));
+};
+
+export const markNotificationsRead = (notificationIds) => (dispatch) => {
+  axios
+    .post("/notifications", notificationIds)
+    .then((res) => {
+      dispatch({
+        type: MARK_NOTIFICATIONS_READ,
+      });
     })
     .catch((err) => console.log(err));
 };
